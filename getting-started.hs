df0840f9ba67493aa0387b2bac7cc58d1905f3af
Innocent <grahamin@60f81db59b6c.ant.amazon.com>
2016-01-20 12:22:41 +0000

    getting started exercises complete

---
+++ getting-started.hs
@@ -0,0 +1,18 @@
+:t 5 + 8
+:t 3 * 5 + 8
+:t 2 + 4
+:t (+) 2 4
+:t sqrt 16
+:t succ 6
+:t succ 7
+:t pred 9
+:t pred 8
+:t sin (pi /2)
+:t truncate pi
+:t round 3.5
+:t round 3.4
+:t floor 3.7
+:t ceiling 3.3
+
+let x = 1
+:show bindings
