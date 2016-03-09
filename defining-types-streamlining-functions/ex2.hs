704193f100523998a35962c548e8cf74420355ee
Innocent <grahamin@60f81db59b6c.ant.amazon.com>
2016-03-09 11:15:13 +0000

    defined maybetree

--- defining-types-streamlining-functions/ex2.hs
+++ defining-types-streamlining-functions/ex2.hs
@@ -1,2 +1,4 @@
-data MaybeTree a = Node a (Maybe (Tree a)) (Maybe (Tree a)) deriving (Show)
+data Tree a = Node a (Tree a) (Tree a) | Empty deriving (Show)
+
+data MaybeTree a = MaybeNode a (Maybe (MaybeTree a)) (Maybe (MaybeTree a)) deriving (Show)
 
